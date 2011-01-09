a15eb3ac317d713251b80012455f19e9535b9dee
Zhaonan Li <paullzn@gmail.com>
2011-01-09 14:24:28 +0000

    trying django-nonrel framework to build the site on google app engine.

---
+++ paullzn-nonrel/guestbook/views.py
@@ -0,0 +1,39 @@
+# Create your views here.
+# Corresponding Django view, "django-guestbook/guestbook/views.py"
+from django.core.cache import cache
+from django.views.generic.simple import direct_to_template
+from guestbook.forms import CreateGreetingForm
+from guestbook.models import Greeting
+
+def list_greetings(request):
+    #greetings = cache.get(MEMCACHE_GREETINGS)
+    #if greetings is None:
+    greetings = Greeting.objects.all().order_by('-date')[:10]
+        #cache.add(MEMCACHE_GREETINGS, greetings)
+    return direct_to_template(request, 'guestbook/index.html',
+        {'greetings': greetings, 'form': CreateGreetingForm()})
+
+from django.http import HttpResponseRedirect
+        
+def create_greeting(request):
+    if request.method == 'POST':
+        greeting = Greeting()
+        greeting.content = request.POST.get('content')
+        if request.user.is_authenticated():
+            greeting.author = request.user
+        greeting.save()
+        cache.delete('greetings')
+    return HttpResponseRedirect('/guestbook/')
+  
+    
+def create_greeting(request):
+    # bound form (user input in request)
+    if request.method == 'POST':
+        form = CreateGreetingForm(request.POST)
+        if form.is_valid():
+            greeting = form.save(commit=False)
+            if request.user.is_authenticated():
+                greeting.author = request.user
+            greeting.save()
+            cache.delete('greetings')
+    return HttpResponseRedirect('/guestbook/')
